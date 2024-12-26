b8a09753694f81cb0c48fbd30b2eb801851e2e3b
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-12-26 09:03:45 +0100

    Ajoutez des fichiers projet.

---
+++ Demo/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Timers;
+
+namespace Demo.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly string email = "contact@bestscore.com";
+        private readonly string address = "New york,USA";
+        public IActionResult Index()
+        {
+            ViewData["EmailAddress"] = email;
+            ViewBag.Address = address;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(ContactDto model)
+        {
+            if(!ModelState.IsValid) {
+                return View();
+            }
+            // Store the data to the database
+            ModelState.Clear();
+            ViewBag.Message = "Your message is recieved successfully!";
+            return View(model);
+        }
+    }
+}
---
+++ Demo/Models/ContactDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Models
+{
+    public class ContactDto
+    {
+        [Required(ErrorMessage ="The first name is required")]
+        public string FirstName { get; set; } = "";
+        [Required(ErrorMessage = "The last Name is required")]
+        public string LastName { get; set; } = "";
+        [Required(ErrorMessage = "The email is required")]
+        [EmailAddress]
+        public string EmailAddress { get; set; } = "";
+
+        [Required(ErrorMessage ="The message is required")]
+        public string Message { get; set; } = "";
+    }
+}
